386e43b5895becaefda576d86089ccd337a9ed5c
anh-ngo <110221356+anh-ngo@users.noreply.github.com>
2022-08-30 18:53:38 +0300

    init, thanks john u r so poot prawwww

---
+++ _site/assets/main.js
@@ -0,0 +1,27 @@
+document.getElementById("expand-on-click").addEventListener("click", function() {
+    document.getElementById("expand-on-click").classList = "hidden";
+    document.getElementById("collapse-on-click").classList = "no-highlight visible-inline";
+
+    let hiddenElements = document.querySelectorAll(".hidden");
+    for (let i = 0; i < hiddenElements.length; i++) {
+        if (hiddenElements[i].tagName == "P") {
+            hiddenElements[i].classList = "visible-block";
+        } else if (hiddenElements[i].tagName == "DIV") {
+            hiddenElements[i].classList = "text-wrapper visible-block";
+        }
+    }
+});
+
+document.getElementById("collapse-on-click").addEventListener("click", function() {
+    document.getElementById("collapse-on-click").classList = "hidden";
+    document.getElementById("expand-on-click").classList = "no-highlight visible-inline";
+
+    let visibleElements = document.querySelectorAll(".visible-block");
+    for (let i = 0; i < visibleElements.length; i++) {
+        if (visibleElements[i].tagName == "P") {
+            visibleElements[i].classList = "hidden";
+        } else if (visibleElements[i].tagName == "DIV") {
+            visibleElements[i].classList = "text-wrapper hidden";
+        }
+    }
+});
